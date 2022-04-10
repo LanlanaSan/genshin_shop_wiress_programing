7b19e4bac06392fb26a4fd4e98f7ee87bd664d7c
LanlanaSan <lanlan.san@student.mahidol.ac.th>
2022-04-10 09:42:14 +0700

    second commit

---
+++ lib/model/productInfo.dart
@@ -0,0 +1,159 @@
+class product {
+  final String Product_Img, Product_Name, Product_Description;
+  final double Product_Price;
+  product({
+    required this.Product_Img,
+    required this.Product_Name,
+    required this.Product_Description,
+    required this.Product_Price,
+  });
+  List<product> products = [
+    product(
+        Product_Name: "Wanderer's String-Kettle Necklace",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/O1CN01zdw7Lz1v7IcOEPHFw__2208460416125_d45f4d27-2a33-4ed1-a205-08d77bfd8964_590x.jpg?v=1642706744",
+        Product_Price: 2091.31,
+        Product_Description:
+            "Material: Plated Copper, Inlaid Zircon\nWeight: Approx. 4.2g \nLength of the Necklace: 420 mm\nLength of the charm: 17.5 X 10 mm"),
+    product(
+        Product_Name: "Bard's Arrow Feather Enamel pin",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/O1CN01SgUiVw1v7IcR0u01r__2208460416125_458b8332-2ba5-4104-83ea-4461de612789_590x.jpg?v=1642706746",
+        Product_Price: 1447.73,
+        Product_Description:
+            "Material:  Plated Copper, Inlaid Zircon\nWeight: Approx. 4.1g \nLength of item: 31.5 X 16 mm"),
+    product(
+        Product_Name: "Concert's Final Hour Adjustable Ring",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/O1CN018loKK81v7IcOqN0ve__2208460416125_29f4e46b-efa8-4bd8-a733-6491a70ff2dc_590x.jpg?v=1642706747",
+        Product_Price: 1447.73,
+        Product_Description:
+            "Material:  Plated Copper, Inlaid Zircon\nWeight: Approx. 4.1g \nInner Diameter: 15.9 ~ 18.2 MM\nInner Circumference: 50 ~ 57 MM"),
+    product(
+        Product_Name: "Diluc Letter Set",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/DilucLetter_590x.jpg?v=1642879789",
+        Product_Price: 578.9,
+        Product_Description:
+            "The Letter Set consists of 3 items\n1. Badge (58mm Diameter, Alloy Metal)\n2. Colour Paper (150 x 150 mm)\n3. Letter Set\n\n\nEnvelope: (110 X 160 mm, 200g card board)\nPost card: (148 X 202 mm) \nSticker: (108 X 75 mm)"),
+    product(
+        Product_Name: "Xiao Letter Set",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/O1CN01yspk091v7IbyHrGug__2208460416125_590x.jpg?v=1642879789",
+        Product_Price: 578.9,
+        Product_Description:
+            "The Letter Set consists of 3 items\n1. Badge (58mm Diameter, Alloy Metal)\n2. Colour Paper (150 x 150 mm)\n3. Letter Set\n\n\nEnvelope: (110 X 160 mm, 200g card board)\nPost card: (148 X 202 mm) \nSticker: (108 X 75 mm)"),
+    product(
+        Product_Name: "Bennett Letter Set",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/O1CN01xQkymC1v7IbzwMYVq__2208460416125_8ca82765-50f3-40ed-a4aa-8714177520c8_590x.jpg?v=1642879789",
+        Product_Price: 578.9,
+        Product_Description:
+            "The Letter Set consists of 3 items\n1. Badge (58mm Diameter, Alloy Metal)\n2. Colour Paper (150 x 150 mm)\n3. Letter Set\n\n\nEnvelope: (110 X 160 mm, 200g card board)\nPost card: (148 X 202 mm) \nSticker: (108 X 75 mm)"),
+    product(
+        Product_Name: "Beidou Letter Set",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/O1CN01RXoLLg1v7IbnnfFSx__2208460416125_27e0134a-4729-4d47-8528-8514862cacc6_590x.jpg?v=1642879789",
+        Product_Price: 578.9,
+        Product_Description:
+            "The Letter Set consists of 3 items\n1. Badge (58mm Diameter, Alloy Metal)\n2. Colour Paper (150 x 150 mm)\n3. Letter Set\n\n\nEnvelope: (110 X 160 mm, 200g card board)\nPost card: (148 X 202 mm) \nSticker: (108 X 75 mm)"),
+    product(
+        Product_Name: "Diona Letter Set",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/O1CN012sssQD1v7Ibj4rdmh__2208460416125_7328723e-718b-4554-aaff-d54d386559ec_590x.jpg?v=1642879789",
+        Product_Price: 578.9,
+        Product_Description:
+            "The Letter Set consists of 3 items\n1. Badge (58mm Diameter, Alloy Metal)\n2. Colour Paper (150 x 150 mm)\n3. Letter Set\n\n\nEnvelope: (110 X 160 mm, 200g card board)\nPost card: (148 X 202 mm) \nSticker: (108 X 75 mm)"),
+    product(
+        Product_Name: "Noelle Letter Set",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/NoelleLetter_590x.jpg?v=1642872696",
+        Product_Price: 578.9,
+        Product_Description:
+            "The Letter Set consists of 3 items\n1. Badge (58mm Diameter, Alloy Metal)\n2. Colour Paper (150 x 150 mm)\n3. Letter Set\n\n\nEnvelope: (110 X 160 mm, 200g card board)\nPost card: (148 X 202 mm) \nSticker: (108 X 75 mm)"),
+    product(
+        Product_Name: "Jean Letter Set",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/JeanLetter_590x.jpg?v=1642872697",
+        Product_Price: 578.9,
+        Product_Description:
+            "The Letter Set consists of 3 items\n1. Badge (58mm Diameter, Alloy Metal)\n2. Colour Paper (150 x 150 mm)\n3. Letter Set\n\n\nEnvelope: (110 X 160 mm, 200g card board)\nPost card: (148 X 202 mm) \nSticker: (108 X 75 mm)"),
+    product(
+        Product_Name: "Qiqi Letter Set",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/QiQiLetter_590x.jpg?v=1642872699",
+        Product_Price: 578.9,
+        Product_Description:
+            "The Letter Set consists of 3 items\n1. Badge (58mm Diameter, Alloy Metal)\n2. Colour Paper (150 x 150 mm)\n3. Letter Set\n\n\nEnvelope: (110 X 160 mm, 200g card board)\nPost card: (148 X 202 mm) \nSticker: (108 X 75 mm)"),
+    product(
+        Product_Name: "Raiden Shogun Standee",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/RaidenShogunStandee_590x.jpg?v=1641831135",
+        Product_Price: 546.72,
+        Product_Description:
+            "Material: Acrylic\nSize: 11 cm wide, 15.5 cm tall"),
+    product(
+        Product_Name: "Kujou Sara Standee",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/KujouSara_590x.jpg?v=1641831135",
+        Product_Price: 546.72,
+        Product_Description:
+            "Material: Acrylic\nSize: 11 cm wide, 15.5 cm tall"),
+    product(
+        Product_Name: "Sangonomiya Kokomi Standee",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/kokomi_590x.jpg?v=1641831135",
+        Product_Price: 546.72,
+        Product_Description:
+            "Material: Acrylic\nSize: 11 cm wide, 15.5 cm tall"),
+    product(
+        Product_Name: "Kamisato Ayaka Standee",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/AyayaStandee_590x.jpg?v=1641831135",
+        Product_Price: 546.72,
+        Product_Description:
+            "Material: Acrylic\nSize: 11 cm wide, 15.5 cm tall"),
+    product(
+        Product_Name: "Sayu Standee",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/SayuStandee_590x.jpg?v=1641831135",
+        Product_Price: 546.72,
+        Product_Description:
+            "Material: Acrylic\nSize: 11 cm wide, 15.5 cm tall"),
+    product(
+        Product_Name: "Yoimiya Standee",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/YomiyaStandee_590x.jpg?v=1641831135",
+        Product_Price: 546.72,
+        Product_Description:
+            "Material: Acrylic\nSize: 11 cm wide, 15.5 cm tall"),
+    product(
+        Product_Name: "Kaedehara Kazuha Standee",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/O1CN01b72Kaz1v7IaiHoVFz__2208460416125_590x.jpg?v=1641831135",
+        Product_Price: 546.72,
+        Product_Description:
+            "Material: Acrylic\nSize: 11 cm wide, 15.5 cm tall"),
+    product(
+        Product_Name: "Arataki Itto Standee",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/itto_standee_590x.jpg?v=1641831135",
+        Product_Price: 546.72,
+        Product_Description:
+            "Material: Acrylic\nSize: 11 cm wide, 15.5 cm tall"),
+    product(
+        Product_Name: "Thoma Standee",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/thomastandee_590x.jpg?v=1641831130",
+        Product_Price: 546.72,
+        Product_Description:
+            "Material: Acrylic\nSize: 11 cm wide, 15.5 cm tall"),
+    product(
+        Product_Name: "Gorou Standee",
+        Product_Img:
+            "https://cdn.shopify.com/s/files/1/0508/7775/9669/products/Goroustandee_590x.jpg?v=1641831130",
+        Product_Price: 546.72,
+        Product_Description:
+            "Material: Acrylic\nSize: 11 cm wide, 15.5 cm tall")
+  ];
+}
